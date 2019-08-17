5aa063920c6c77d03964c7fe0277fbcb6edde9bd
Chinhtv <chinhtv18@gmail.com>
2019-08-17 17:41:53 +0700

    Add UT LoginApi

---
+++ tests/Feature/LoginApiTest.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Http\Response;
+
+class LoginApiTest extends TestCase
+{
+    const SUCCESS_STATUS = 'success';
+    const ERROR_STATUS = 'fail';
+    protected $apiStructure = [
+        'status',
+        'message',
+        'data' => []
+    ];
+    /**
+     * @test
+     *
+     */
+    public function urlReturnOK()
+    {
+        $response = $this->post('/api/login', [
+            "email" => "admin@gmail.com",
+            "password" => "123"
+        ]);
+
+        $response->assertStatus(Response::HTTP_OK);
+    }
+
+    public function testLoginSuccess()
+    {
+        $response = $this->post('/api/login', [
+            "email" => "admin@gmail.com",
+            "password" => "123"
+        ]);
+
+        $response->assertStatus(Response::HTTP_OK);
+        $response->assertJsonStructure($this->apiStructure);
+        $responseData = $response->json();
+        $this->assertEquals($responseData['status'], self::SUCCESS_STATUS);
+        $this->assertNotEmpty($responseData['data']['access_token']);
+        $this->assertNotEmpty($responseData['data']['userInfo']);
+    }
+
+    public function testLoginWithWrongEmail()
+    {
+        $response = $this->post('/api/login', [
+            "email" => "admin123@vti.test",
+            "password" => "123"
+        ]);
+        $response->assertStatus(Response::HTTP_OK);
+        $response->assertJsonStructure($this->apiStructure);
+        $responseData = $response->json();
+        $this->assertEquals($responseData['status'], self::ERROR_STATUS);
+        $this->assertArrayNotHasKey('userInfo', $responseData['data']);
+    }
+
+    public function testLoginWithWrongPassword()
+    {
+        $response = $this->post('/api/login', [
+            "email" => "admin@vti.test",
+            "password" => "1234"
+        ]);
+        $response->assertStatus(Response::HTTP_OK);
+        $response->assertJsonStructure($this->apiStructure);
+        $responseData = $response->json();
+        $this->assertEquals($responseData['status'], self::ERROR_STATUS);
+        $this->assertArrayNotHasKey('userInfo', $responseData['data']);
+    }
+}
