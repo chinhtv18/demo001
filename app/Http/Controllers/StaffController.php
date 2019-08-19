d4821e2a6041e7044c6aa10d103b5bfc1b980aa6
Chinhtv <chinhtv18@gmail.com>
2019-08-19 23:12:10 +0700

    Implement CRUD Staff

---
+++ app/Http/Controllers/StaffController.php
@@ -0,0 +1,107 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Staff;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Validator;
+
+class StaffController extends Controller
+{
+   public function getStaffs()
+   {
+       try {
+           $staffs = Staff::query()->get();
+           return $this->apiResponse($staffs, 'Get staff list success');
+       } catch (\Exception $ex) {
+           return response()->json($ex->getMessage(), Response::HTTP_INTERNAL_SERVER_ERROR);
+       }
+   }
+
+   public function getStaffDetail(Request $request, $id)
+   {
+       try {
+            $staff = Staff::query()->find($id);
+            if (empty($staff)) {
+                return $this->apiResponse([], 'Staff not found', self::ERROR_STATUS);
+            }
+
+            return $this->apiResponse($staff, 'Get staff detail success', self::SUCCESS_STATUS);
+
+       } catch (\Exception $ex) {
+           return response()->json($ex->getMessage(), Response::HTTP_INTERNAL_SERVER_ERROR);
+       }
+   }
+
+   public function createStaff(Request $request)
+   {
+        try {
+            $data = $request->all();
+            $validator = $this->makeValidator($data);
+            if ($validator->fails()) {
+                $messages = $this->responseMessage($validator->errors()->toArray());
+                return $this->apiResponse([], $messages, parent::ERROR_STATUS, Response::HTTP_UNPROCESSABLE_ENTITY);
+            }
+            $this->storeStaff($data);
+
+        } catch (\Exception $ex) {
+            return response()->json($ex->getMessage(), Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
+   }
+
+   public function updateStaff(Request $request, $id)
+   {
+       try {
+           $data = $request->all();
+           $validator = $this->makeValidator($data);
+           if ($validator->fails()) {
+               $messages = $this->responseMessage($validator->errors()->toArray());
+               return $this->apiResponse([], $messages, parent::ERROR_STATUS, Response::HTTP_UNPROCESSABLE_ENTITY);
+           }
+           $this->storeStaff($data, $id);
+
+       } catch (\Exception $ex) {
+           return response()->json($ex->getMessage(), Response::HTTP_INTERNAL_SERVER_ERROR);
+       }
+   }
+
+   public function storeStaff($data, $id = null)
+   {
+       if (!empty($id)) {
+           $staffUpdate = Staff::query()->where('id', $id)->first();
+           if (empty($staffUpdate)) {
+               return $this->apiResponse([], 'Staff not found', parent::ERROR_STATUS);
+           }
+           $result = $staffUpdate->update($data);
+           $message = 'update staff success';
+       } else {
+           $result = Staff::create($data);
+           $message = 'create staff success';
+       }
+       return $this->apiResponse($result, $message, parent::SUCCESS_STATUS);
+   }
+
+   public function deleteStaff(Request $request, $id)
+   {
+       try {
+           $staffDelete = Staff::query()->where('id', $id)->first();
+           if (empty($staffDelete)) {
+               return $this->apiResponse([], 'Staff not found', parent::ERROR_STATUS);
+           }
+           $staffDelete->delete();
+           return $this->apiResponse([], 'Delete customer success', parent::SUCCESS_STATUS);
+       } catch (\Exception $ex) {
+           return response()->json($ex->getMessage(), Response::HTTP_INTERNAL_SERVER_ERROR);
+       }
+   }
+   private function makeValidator($data) {
+       return Validator::make($data, [
+           'email' => 'required|unique:staffs|email',
+           'password' => 'required',
+           'first_name' => 'required',
+           'last_name' => 'required'
+       ]);
+   }
+
+}
